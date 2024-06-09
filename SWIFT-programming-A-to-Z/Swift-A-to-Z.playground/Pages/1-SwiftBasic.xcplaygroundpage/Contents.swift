426faa9d6e6b2c4abb1da33c4c784fca348eca07
chamudithaperera <chamudithakavishan2001@gmail.com>
2024-06-09 13:17:06 +0530

    first commit

---
+++ SWIFT-programming-A-to-Z/Swift-A-to-Z.playground/Pages/1-SwiftBasic.xcplaygroundpage/Contents.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+//you can add single line comment using double forward slashs
+
+/*
+    This is multi-line comment.
+*/
+
+//print - you can print or display anything using print keyword
+print("Hello Swift")
+
+//Naming Convertions
+//Camel Case
+let firstGreet = "Hello Samel Case"
+
+//Snake Case
+let first_Greet = "Hello Snake Case"
+
+//Pascal Case
+let FirstGreet = "Hello Snake Case"
---
+++ SWIFT-programming-A-to-Z/Swift-A-to-Z.playground/Pages/2-SwiftTypes.xcplaygroundpage/Contents.swift
@@ -0,0 +1,23 @@
+import Foundation
+
+//String
+let item1 = "Hello String"
+let item2: String = "Hello String"
+
+//Boolean
+let item3 = true
+let item4: Bool = false
+
+//Date
+let date1: Date = Date()
+
+//Int
+let item5 = 10
+let item6: Int = 20
+
+//Float , Double
+let item7 = 10.5
+let item8: CGFloat = 20.5
+
+let item9 = 10.5
+let item10: Double = 20.5
